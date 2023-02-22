eb2216300bfe3d7f996ed07ac07eff5b3f3b1160
Jamison Fox <fox.jamison@outlook.com>
2023-02-22 16:35:31 -0700

    completed guided project

---
+++ api/auth/auth-middleware.js
@@ -0,0 +1,13 @@
+// const db = 
+
+async function protect(req, res, next) {
+    if (req.session.user) {
+        next()
+    } else {
+        next({ status:401, message: 'you shall not pass!!!' })
+    }
+}
+
+module.exports = {
+    protect,
+}
